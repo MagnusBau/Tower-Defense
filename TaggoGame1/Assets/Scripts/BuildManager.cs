06ff78e7f8901095bdca309f6d5679758bb8a626
MagnusBau <magnus.baugerd@hotmail.com>
2020-06-11 12:53:36 +0200

    Working on ability to plave turrets

---
+++ TaggoGame1/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+    public GameObject DefaultBuilding;
+    private GameObject BuildingToBuild;
+
+    public GameObject GetBuildingToBuild()
+    {
+        return BuildingToBuild;
+    }
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one Build Manager!");
+        }
+    }
+
+    private void Start()
+    {
+        BuildingToBuild = DefaultBuilding;
+    }
+}
+
--- TaggoGame1/Assets/Scripts/Node.cs
+++ TaggoGame1/Assets/Scripts/Node.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Color hoverColor;
+    public Vector3 offset;
+
+    private GameObject building;
+    private Renderer rend;
+    private Color defaultColor;
+
+    private void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        defaultColor = rend.material.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseEnter()
     {
-        
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = defaultColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if(building != null)
+        {
+            return;
+        }
+        GameObject buildingToBuild = BuildManager.instance.GetBuildingToBuild();
+        building = (GameObject)Instantiate(buildingToBuild, transform.position + offset, transform.rotation);
     }
 }
