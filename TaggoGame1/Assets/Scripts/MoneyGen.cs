f958355e682101cc78bb7a03b182f332130fa6b1
Asbjørn Fiksdal Kallestad <asbjorfk@stud.ntnu.no>
2020-06-23 20:08:23 +0200

    re-wrote because push; git-delete my files :(

--- TaggoGame1/Assets/Scripts/MoneyGen.cs
+++ TaggoGame1/Assets/Scripts/MoneyGen.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MoneyGen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float amountPerGeneration = 69f;
+    public float generationRate = 0.3f;
+    public float cooldown;
+    BuildManager buildManager;
+
     void Start()
     {
-        
+        buildManager = BuildManager.instance;
+        cooldown = 1 / generationRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0f)
+        {
+            GenerateMoney();
+        }
+    }
+
+    public void GenerateMoney()
+    {
+        buildManager.AddCurrency(amountPerGeneration);
+        cooldown = 1 / generationRate;
+        Debug.Log("Building money has been gen");
     }
 }
