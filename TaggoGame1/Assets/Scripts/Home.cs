67956226ea27eaefa5194cd0a573e6e5d58cc14f
MagnusBau <magnus.baugerd@hotmail.com>
2020-06-23 18:33:52 +0200

    Added functioning currency and moneygeneration for Home

--- TaggoGame1/Assets/Scripts/Home.cs
+++ TaggoGame1/Assets/Scripts/Home.cs
@@ -5,6 +5,10 @@ public class Home : MonoBehaviour
 {
     public float hp = 15f;
     public Color destroyedColor;
+    public float amountPerGeneration = 100f;
+    public float generationRate = 0.5f;
+    public float cooldown;
+    BuildManager buildManager;
 
 
     public Text hpCounterText;
@@ -12,6 +16,17 @@ public class Home : MonoBehaviour
     void Start()
     {
         hpCounterText.text = hp.ToString();
+        buildManager = BuildManager.instance;
+        cooldown = 1 / generationRate;
+    }
+
+    private void Update()
+    {
+        cooldown -= Time.deltaTime;
+        if(cooldown <= 0f)
+        {
+            GenerateMoney();
+        }
     }
     public void Damage(float points)
     {
@@ -24,4 +39,10 @@ public class Home : MonoBehaviour
         hpCounterText.text = hp.ToString();
     }
 
+    public void GenerateMoney()
+    {
+        buildManager.AddCurrency(amountPerGeneration);
+        cooldown = 1 / generationRate;
+        Debug.Log("Money!");
+    }
 }
--- TaggoGame1/Assets/Scripts/stronkEnemy.cs
+++ TaggoGame1/Assets/Scripts/stronkEnemy.cs
@@ -1,12 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿
 public class StronkEnemy : Enemy
 {
-    void Start()
-    {
-        SetAttributes(4f, 2f);
-        FindDestination();
-    }
+
 }
